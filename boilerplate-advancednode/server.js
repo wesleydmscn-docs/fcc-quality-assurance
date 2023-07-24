e9cb9f605b9a4ca9319cc8f0b337e799a997bd22
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-07-24 07:00:23 -0300

    docs(advanced-node): Set up Passport

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -5,6 +5,8 @@ const myDB = require("./connection")
 const fccTesting = require("./freeCodeCamp/fcctesting.js")
 
 const app = express()
+const session = require("express-session")
+const passport = require("passport")
 
 app.set("view engine", "pug")
 app.set("views", "./views/pug")
@@ -13,6 +15,18 @@ fccTesting(app) //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" })
