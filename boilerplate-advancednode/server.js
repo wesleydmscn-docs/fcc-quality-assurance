b1625d5e18339e48d1798822f2e91bad2f2e9ea7
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-07-26 18:07:03 -0300

    docs(advanced-node): How to Put a Profile Together

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -72,7 +72,9 @@ myDB(async (client) => {
     )
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile")
+    res.render("profile", {
+      username: req.user.username,
+    })
   })
 
   passport.serializeUser((user, done) => {
