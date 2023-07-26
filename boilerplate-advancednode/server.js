1ca9a8631014394bde6ab69dc61b2e450df202b9
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-07-26 18:48:27 -0300

    docs(advanced-node): Registration of New Users

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -60,6 +60,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please login",
       showLogin: true,
+      showRegistration: true,
     })
   })
 
@@ -81,6 +82,42 @@ myDB(async (client) => {
     res.render("profile", {
       username: req.user.username,
     })
+  })
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err)
+        } else if (user) {
+          res.redirect("/")
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/")
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0])
+              }
+            }
+          )
+        }
+      })
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile")
+    }
+  )
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found")
   })
 
   passport.serializeUser((user, done) => {
