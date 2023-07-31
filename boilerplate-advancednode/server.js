f0c32389fa6878282643a4deb0e8d6b0ee0e088b
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-07-31 09:22:24 -0300

    docs(advanced-node): Clean Up Your Project with Modules

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -1,26 +1,19 @@
 "use strict"
+
 require("dotenv").config()
 
 const express = require("express")
 const myDB = require("./connection")
 const fccTesting = require("./freeCodeCamp/fcctesting.js")
-
-const app = express()
 const session = require("express-session")
 const passport = require("passport")
+const routes = require("./routes.js")
+const auth = require("./auth.js")
 
-const LocalStrategy = require("passport-local")
-const bcrypt = require("bcrypt")
-
-const { ObjectID } = require("mongodb")
+const app = express()
 
 app.set("view engine", "pug")
 app.set("views", "./views/pug")
-
-fccTesting(app) //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 app.use(
   session({
@@ -34,106 +27,16 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+fccTesting(app) // For fCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users")
 
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect("/")
-  }
-
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      myDataBase.findOne({ username: username }, (err, user) => {
-        console.log(`User ${username} attempted to log in.`)
-        if (err) return done(err)
-        if (!user) return done(null, false)
-        if (!bcrypt.compareSync(password, user.password)) {
-          return done(null, false)
-        }
-        return done(null, user)
-      })
-    })
-  )
-
-  app.route("/").get((req, res) => {
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please login",
-      showLogin: true,
-      showRegistration: true,
-    })
-  })
-
-  app
-    .route("/login")
-    .post(
-      passport.authenticate("local", { failureRedirect: "/" }),
-      (req, res) => {
-        res.redirect("/profile")
-      }
-    )
-
-  app.route("/logout").get((req, res) => {
-    req.logout()
-    res.redirect("/")
-  })
-
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", {
-      username: req.user.username,
-    })
-  })
-
-  app.route("/register").post(
-    (req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        const hash = bcrypt.hashSync(req.body.password, 12)
-
-        if (err) {
-          next(err)
-        } else if (user) {
-          res.redirect("/")
-        } else {
-          myDataBase.insertOne(
-            {
-              username: req.body.username,
-              password: hash,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/")
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0])
-              }
-            }
-          )
-        }
-      })
-    },
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res, next) => {
-      res.redirect("/profile")
-    }
-  )
-
-  app.use((req, res, next) => {
-    res.status(404).type("text").send("Not Found")
-  })
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id)
-  })
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc)
-    })
-  })
+  routes(app, myDataBase)
+  auth(app, myDataBase)
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("index", { title: e, message: "Unable to connect to database" })
@@ -142,5 +45,5 @@ myDB(async (client) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT)
+  console.log(`Listening on port ${PORT}`)
 })
