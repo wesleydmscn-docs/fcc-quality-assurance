35afb1463b048fd8794da2908470d56043e1475d
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-10-30 16:11:33 -0300

    metric-imperial-converter: add unit/functional tests from boilerplate

--- certification-projects/boilerplate-project-metricimpconverter/tests/2_functional-tests.js
+++ certification-projects/boilerplate-project-metricimpconverter/tests/2_functional-tests.js
@@ -5,4 +5,39 @@ const server = require("../server")
 
 chai.use(chaiHttp)
 
-suite("Functional Tests", function () { })
+suite("Functional Tests", function () {
+  suite("Routing Tests", function () {
+    suite("GET /api/convert => conversion object", function () {
+      test("Convert 10L (valid input)", function (done) {
+        chai
+          .request(server)
+          .get("/api/convert")
+          .query({ input: "10L" })
+          .end(function (err, res) {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 10)
+            assert.equal(res.body.initUnit, "l")
+            assert.approximately(res.body.returnNum, 2.64172, 0.1)
+            assert.equal(res.body.returnUnit, "gal")
+            done()
+          })
+      })
+
+      test("Convert 32g (invalid input unit)", function (done) {
+        //done()
+      })
+
+      test("Convert 3/7.2/4kg (invalid number)", function (done) {
+        //done()
+      })
+
+      test("Convert 3/7.2/4kilomegagram (invalid number and unit)", function (done) {
+        //done()
+      })
+
+      test("Convert kg (no number)", function (done) {
+        //done()
+      })
+    })
+  })
+})
