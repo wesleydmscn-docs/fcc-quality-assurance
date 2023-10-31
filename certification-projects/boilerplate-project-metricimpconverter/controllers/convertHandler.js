fd8ab591e45b270891e485bdf7d95cae508f9015
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-10-31 17:39:28 -0300

    metric-imperial-converter: implement all convertHandler functions

--- certification-projects/boilerplate-project-metricimpconverter/controllers/convertHandler.js
+++ certification-projects/boilerplate-project-metricimpconverter/controllers/convertHandler.js
@@ -1,35 +1,78 @@
 function ConvertHandler() {
   this.getNum = function (input) {
-    let result
+    let result = input.split(/[^0-9|/.$]/g).join("")
 
-    return input
+    if (result.includes("/") && result.split("/").length === 2) {
+      const [number1, number2] = result.split("/")
+
+      result = Number(number1) / Number(number2)
+
+      return result
+    }
+
+    if (result.includes("/") && result.split("/").length > 2) {
+      throw new Error("Error on a double-fraction")
+    }
+
+    if (Number(result) && !isNaN(result)) {
+      return result
+    }
+
+    return 1
   }
 
   this.getUnit = function (input) {
-    let result
+    let result = input.split(/[0-9]/g).join("")
 
     return result
   }
 
   this.getReturnUnit = function (initUnit) {
-    let result
+    const units = {
+      "gal": "L",
+      "l": "gal",
+      "mi": "km",
+      "km": "mi",
+      "lbs": "kg",
+      "kg": "lbs",
+    }
 
-    return result
+    return units[initUnit]
   }
 
   this.spellOutUnit = function (unit) {
-    let result
+    const units = {
+      "gal": "Gallon",
+      "l": "Litre",
+      "mi": "Mile",
+      "km": "Kilometers",
+      "lbs": "Pounds",
+      "kg": "Kilo",
+    }
 
-    return result
+    return units[unit]
   }
 
   this.convert = function (initNum, initUnit) {
     const galToL = 3.78541
-    const lbsToKg = 0.453592
-    const miToKm = 1.60934
-    let result
+    const lToGal = 0.26417
 
-    return result
+    const lbsToKg = 0.453592
+    const kgToLbs = 2.204622
+
+    const miToKm = 1.60934
+    const kmToMi = 0.62137
+
+    const conversions = {
+      "gal": initNum * galToL,
+      "l": initNum * lToGal,
+      "mi": initNum * miToKm,
+      "km": initNum * kmToMi,
+      "lbs": initNum * lbsToKg,
+      "kg": initNum * kgToLbs,
+    }
+
+    return conversions[initUnit]
   }
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
