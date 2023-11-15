b275406d2c5ccb83e5c5f1ba3ae88d88a80b638c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-15 18:25:49 -0300

    sudoku-solver(tests/unit): add to solver variable a Solver class instance

--- certification-projects/boilerplate-project-sudoku-solver/tests/1_unit-tests.js
+++ certification-projects/boilerplate-project-sudoku-solver/tests/1_unit-tests.js
@@ -2,7 +2,7 @@ const chai = require("chai")
 const assert = chai.assert
 
 const Solver = require("../controllers/sudoku-solver.js")
-let solver
+let solver = new Solver()
 
 suite("Unit Tests", () => {
   const input =
