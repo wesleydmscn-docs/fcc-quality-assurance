997a22682d9f600e36e79b833211d62d3e150618
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-15 17:59:20 -0300

    sudoku-solver(tests/unit): implement 'Handles valid string'

--- certification-projects/boilerplate-project-sudoku-solver/tests/1_unit-tests.js
+++ certification-projects/boilerplate-project-sudoku-solver/tests/1_unit-tests.js
@@ -4,4 +4,11 @@ const assert = chai.assert
 const Solver = require("../controllers/sudoku-solver.js")
 let solver
 
-suite("Unit Tests", () => { })
+suite("Unit Tests", () => {
+  const input =
+    ".6.3.9..5.74.251..5..1.6.49..7..1.6.34....79....................1.....2....63..5."
+
+  test("Handles valid string", () => {
+    assert.isUndefined(solver.validate(input), "no error object should return")
+  })
+})
