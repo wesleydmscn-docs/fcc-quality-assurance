446b0a92182af30e2709e55c7a1932feda3f68be
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-15 18:14:38 -0300

    sudoku-solver(tests/functional): implement 'POST /api/solve with valid string'

--- certification-projects/boilerplate-project-sudoku-solver/tests/2_functional-tests.js
+++ certification-projects/boilerplate-project-sudoku-solver/tests/2_functional-tests.js
@@ -5,4 +5,22 @@ const server = require("../server")
 
 chai.use(chaiHttp)
 
-suite("Functional Tests", () => { })
+suite("Functional Tests", () => {
+  const puzzle =
+    ".6.3.9..5.74.251..5..1.6.49..7..1.6.34....79....................1.....2....63..5."
+
+  test("POST /api/solve with valid string", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({ puzzle: puzzle })
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(
+          res.body.solution,
+          "162349875974825136583176249857491362341562798296783514635214987418957623729638451"
+        )
+        done()
+      })
+  })
+})
