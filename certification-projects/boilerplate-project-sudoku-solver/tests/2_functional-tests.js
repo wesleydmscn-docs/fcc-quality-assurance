8dd877f6b40f65d1689506426c300cd1f733ef26
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-15 18:16:18 -0300

    sudoku-solver(tests/functional): implement 'POST /api/solve with incorrect length'

--- certification-projects/boilerplate-project-sudoku-solver/tests/2_functional-tests.js
+++ certification-projects/boilerplate-project-sudoku-solver/tests/2_functional-tests.js
@@ -49,4 +49,19 @@ suite("Functional Tests", () => {
         done()
       })
   })
+
+  test("POST /api/solve with incorrect length", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          ".6.3.9..5.74.251..5..1.6.49..7..1.6.34....79...................1.....2....63..5.",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(res.body.error, "Expected puzzle to be 81 characters long")
+        done()
+      })
+  })
 })
