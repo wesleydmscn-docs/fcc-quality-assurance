11b32f116ce3f411ba1798b38bf7e5de79fbc11c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-16 08:57:43 -0300

    american-british-en-translator(tests/unit): instance the Translator class

--- certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
+++ certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
@@ -2,6 +2,7 @@ const chai = require("chai")
 const assert = chai.assert
 
 const Translator = require("../components/translator.js")
+const translator = new Translator()
 
 suite("Unit Tests", () => {
   suite("American English To British English", () => {
