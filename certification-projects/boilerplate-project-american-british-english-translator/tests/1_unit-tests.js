e5809666dd89347e0c743ec6e878c39412d49d35
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-16 08:55:21 -0300

    american-british-en-translator(tests/unit): create suite blocks

--- certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
+++ certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
@@ -3,4 +3,16 @@ const assert = chai.assert
 
 const Translator = require("../components/translator.js")
 
-suite("Unit Tests", () => { })
+suite("Unit Tests", () => {
+  suite("American English To British English", () => {
+
+  })
+
+  suite("British English To American English", () => {
+
+  })
+
+  suite("Highlight Translations", () => {
+
+  })
+})
