a6ff353688f156a957e9f0c674641e4fa4aac06d
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-16 10:07:31 -0300

    american-british-en-translator(tests/functional): implement 'Translation with text and locale fields: POST request to /api/translate'

--- certification-projects/boilerplate-project-american-british-english-translator/tests/2_functional-tests.js
+++ certification-projects/boilerplate-project-american-british-english-translator/tests/2_functional-tests.js
@@ -7,4 +7,24 @@ chai.use(chaiHttp)
 
 let Translator = require("../components/translator.js")
 
-suite("Functional Tests", () => { })
+suite("Functional Tests", () => {
+  test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .set("content-type", "application/json")
+      .send({
+        text: "I ate yogurt for breakfast.",
+        locale: "american-to-british",
+      })
+      .end((err, response) => {
+        assert.equal(response.body.text, "I ate yogurt for breakfast.")
+        assert.equal(
+          response.body.translation,
+          'I ate <span class="highlight">yoghurt</span> for breakfast.'
+        )
+
+        done()
+      })
+  })
+})
