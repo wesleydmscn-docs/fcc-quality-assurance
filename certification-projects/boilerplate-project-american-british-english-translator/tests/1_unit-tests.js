9bee1f2cfdb239cb47a6b300d11d20234ea80539
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-16 08:59:39 -0300

    american-british-en-translator(tests/unit): implement 'Translate Mangoes are my favorite fruit. to British English'

--- certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
+++ certification-projects/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
@@ -6,14 +6,17 @@ const translator = new Translator()
 
 suite("Unit Tests", () => {
   suite("American English To British English", () => {
+    test("Translate Mangoes are my favorite fruit. to British English", (done) => {
+      assert.equal(
+        translator.americanToBritish("Mangoes are my favorite fruit."),
+        'Mangoes are my <span class="highlight">favourite</span> fruit.'
+      )
 
+      done()
+    })
   })
 
-  suite("British English To American English", () => {
+  suite("British English To American English", () => { })
 
-  })
-
-  suite("Highlight Translations", () => {
-
-  })
+  suite("Highlight Translations", () => { })
 })
